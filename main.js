978bdd669450d6f7946604f41fe9332f88cfd2c0
Anqi-Tech <anqihu1107@gmail.com>
2026-04-24 13:19:54 -0400

    exercise 4

--- main.js
+++ main.js
@@ -14,6 +14,7 @@ const router = createRouter({
             component: loadComponent("chat"),
             props: true,
         },
+        { path: "/profile", component: loadComponent("profile") },
     ],
 });
 
